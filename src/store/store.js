10a7b9d28e9ce265bfe6a4f549bc11bf8904a9be
DESKTOP-FHT2P65\Elena <hellens.rez15@gmail.com>
2022-05-08 21:52:01 +0300

    deleted trash

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import profileReducer from '../slices/profileSlice'
 import postsReducer from './../slices/postsSlice'
-import thunk from 'redux-thunk';
-import { applyMiddleware } from 'redux';
 
 export const store = configureStore({
-    reducer: {
-      posts: postsReducer,
-      profile: profileReducer
-    },
-   // middleware: applyMiddleware(thunk),
-  })
+  reducer: {
+    posts: postsReducer,
+    profile: profileReducer
+  }
+})
